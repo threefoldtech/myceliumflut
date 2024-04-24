6df8b8d572ba6cf4bcd28d004cccaf9fbc0f8945
Iwan BK <iwanbk@gmail.com>
2024-04-24 11:13:03 +0700

    initial ios work

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -5,8 +5,21 @@ import Flutter
 @objc class AppDelegate: FlutterAppDelegate {
   override func application(
     _ application: UIApplication,
-    didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
-  ) -> Bool {
+    didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+
+    let controller : FlutterViewController = window?.rootViewController as! FlutterViewController
+    let tunChannel = FlutterMethodChannel(name: "tech.threefold.mycelium/tun",
+                                              binaryMessenger: controller.binaryMessenger)
+    tunChannel.setMethodCallHandler({
+        [weak self] (call: FlutterMethodCall, result: FlutterResult) -> Void in
+          // This method is invoked on the UI thread.
+          guard call.method == "getBatteryLevel" else {
+            result(FlutterMethodNotImplemented)
+            return
+          }
+          result(90)
+    })
+
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
