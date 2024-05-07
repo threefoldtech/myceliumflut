c090d7ebad732d5f31b3ea12d11fa10218776684
Iwan BK <iwanbk@gmail.com>
2024-05-07 14:34:45 +0700

    code for the tunnel provider

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,31 +1,114 @@
 import UIKit
 import Flutter
+import NetworkExtension
+import Foundation
 
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
-  override func application(
-    _ application: UIApplication,
-    didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+    var vpnManager: NETunnelProviderManager = NETunnelProviderManager()
+    let componentName = "tech.threefold.mycelium.extension"
 
-    let controller : FlutterViewController = window?.rootViewController as! FlutterViewController
-    let tunChannel = FlutterMethodChannel(name: "tech.threefold.mycelium/tun",
-                                              binaryMessenger: controller.binaryMessenger)
-    tunChannel.setMethodCallHandler({
-        [weak self] (call: FlutterMethodCall, result: FlutterResult) -> Void in
-          // This method is invoked on the UI thread.
-        switch call.method {
-        case "getBatteryLevel":
-            result(10)
-        case "startVpn":
-            result(true)
-        case "getTunFD":
-            result(1)
-        default:
-            result(FlutterMethodNotImplemented)
+    override func application(
+        _ application: UIApplication,
+        didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+            let controller : FlutterViewController = window?.rootViewController as! FlutterViewController
+            let tunChannel = FlutterMethodChannel(name: "tech.threefold.mycelium/tun",
+                                                  binaryMessenger: controller.binaryMessenger)
+            tunChannel.setMethodCallHandler({
+                [weak self] (call: FlutterMethodCall, result: FlutterResult) -> Void in
+                // This method is invoked on the UI thread.
+                switch call.method {
+                case "getBatteryLevel":
+                    result(90)
+                case "startVpn":
+                    result(true)
+                case "getTunFD":
+                    result(1)
+                default:
+                    result(FlutterMethodNotImplemented)
+                }
+            })
+            NSLog("iwanbk1 flutter app")
+
+            GeneratedPluginRegistrant.register(with: self)
+            //self.vpnTunnelProviderManagerInit()
+            self.initTunnel()
+
+            return super.application(application, didFinishLaunchingWithOptions: launchOptions)
         }
-    })
 
-    GeneratedPluginRegistrant.register(with: self)
-    return super.application(application, didFinishLaunchingWithOptions: launchOptions)
-  }
+    func initTunnel() {
+        NETunnelProviderManager.loadAllFromPreferences { (providers: [NETunnelProviderManager]?, error: Error?) in
+
+            if let error = error {
+                NSLog("iwanbk1 loadAllFromPref failed:" + error.localizedDescription)
+                return
+            }
+
+            guard let providers = providers else {
+                NSLog("iwanbk1 caught by the guard")
+                return
+            } // Handle error if nil
+
+            NSLog("iwanbk1 passed the guard")
+
+            NSLog("iwanbk1 number of providers : %d", providers.count)
+
+            let myProvider = providers.first(where: { $0.protocolConfiguration?.username == "" }) // Replace with your identifier
+
+            NSLog("iwanbk1 enabling provider")
+            myProvider?.isEnabled = true
+            NSLog("iwanbk1 passed myprovider isEnabled")
+            do {
+                NSLog("iwanbk1 connection.startVPNTUnnel")
+                try myProvider?.connection.startVPNTunnel()
+            } catch {
+                print(error)
+                NSLog("iwanbk1 start vpn tunnel failed" )
+            }
+
+            /*myProvider?.saveToPreferences { error in
+             if let error = error {
+             NSLog("iwanbk1 failed to save preference %s", error.localizedDescription)
+             } else {
+             NSLog("iwanbk1 saveToPreference ga error")
+             do {
+             NSLog("iwanbk1 connection.startVPNTUnnel")
+             try myProvider?.connection.startVPNTunnel()
+             } catch {
+             print(error)
+             NSLog("iwanbk1 start vpn tunnel failed" )
+             }
+             }
+             }*/
+        }
+    }
+    func vpnTunnelProviderManagerInit() {
+        NETunnelProviderManager.loadAllFromPreferences { (savedManagers: [NETunnelProviderManager]?, error: Error?) in
+            if let error = error {
+                NSLog("iwanbk1 loadAllFromPref failed:" + error.localizedDescription)
+            } else {
+                NSLog("iwanbk1 looking for savedManager")
+
+                if let savedManagers = savedManagers {
+                    for manager in savedManagers {
+                        NSLog("found some saved manager")
+                        if (manager.protocolConfiguration as? NETunnelProviderProtocol)?.providerBundleIdentifier == self.componentName {
+                            NSLog("iwanbk found saved vpn manager")
+                            self.vpnManager = manager
+                        }
+                    }
+                }
+
+                // start here
+                self.vpnManager.isEnabled = true
+                do {
+                    try self.vpnManager.connection.startVPNTunnel()
+                } catch {
+                    print(error)
+                    NSLog("start vpn tunnel failed")
+                }
+            }
+        }
+    }
 }
