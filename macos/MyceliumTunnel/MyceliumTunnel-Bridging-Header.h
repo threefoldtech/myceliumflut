73c0f11dc6568f429f3f60f801e9445eb8c730f3
Iwan BK <iwanbk@gmail.com>
2024-09-01 15:04:13 +0700

    tunnel and main compiled

---
+++ macos/MyceliumTunnel/MyceliumTunnel-Bridging-Header.h
@@ -0,0 +1,13 @@
+//
+//  MyceliumTunnel-Bridging-Header.h
+//  MyceliumTunnel
+//
+//  Created by Iwan BK on 01/09/24.
+//
+
+#ifndef MyceliumTunnel_Bridging_Header_h
+#define MyceliumTunnel_Bridging_Header_h
+
+#import "PacketTunnelProvider.h"
+
+#endif /* MyceliumTunnel_Bridging_Header_h */
