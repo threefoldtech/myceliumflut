27ab29769f0cb046700fc7c67290f9124d00973c
Iwan BK <iwanbk@gmail.com>
2024-09-05 21:46:08 +0700

    network sys ext

---
+++ macos/MyceliumTunnel/main.swift
@@ -0,0 +1,15 @@
+//
+//  main.swift
+//  MyceliumTunnel
+//
+//  Created by Iwan BK on 05/09/24.
+//
+
+import Foundation
+import NetworkExtension
+
+autoreleasepool {
+    NEProvider.startSystemExtensionMode()
+}
+
+dispatchMain()
