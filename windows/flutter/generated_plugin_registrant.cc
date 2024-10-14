dc6d2b6f1e2a38303bc6a339c6b4ff888076a67d
Iwan BK <iwanbk@gmail.com>
2024-10-14 10:32:44 +0700

    fix(windows): add close handler.
    add windows close handler with flutter_windows_close package.
    
    The previous handler with flutter_desktop_sleep doesn't work on windows

--- windows/flutter/generated_plugin_registrant.cc
+++ windows/flutter/generated_plugin_registrant.cc
@@ -7,8 +7,11 @@
 #include "generated_plugin_registrant.h"
 
 #include <flutter_desktop_sleep/flutter_desktop_sleep_plugin_c_api.h>
+#include <flutter_window_close/flutter_window_close_plugin.h>
 
 void RegisterPlugins(flutter::PluginRegistry* registry) {
   FlutterDesktopSleepPluginCApiRegisterWithRegistrar(
       registry->GetRegistrarForPlugin("FlutterDesktopSleepPluginCApi"));
+  FlutterWindowClosePluginRegisterWithRegistrar(
+      registry->GetRegistrarForPlugin("FlutterWindowClosePlugin"));
 }
